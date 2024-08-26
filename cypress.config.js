eafde755d5587e0a7f163a84690eecfd0fe58cda
unknown <Matheus@INF-3052.dbserver.com.br>
2024-08-26 18:16:47 -0300

    Projeto base

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
